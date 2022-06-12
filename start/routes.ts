65c6b087dedd43bd349e0c16d3ad67a12b4a9cf7
Pedro <contato.pedromoc@gmail.com>
2022-06-11 22:51:19 -0300

    Connect with database

--- start/routes.ts
+++ start/routes.ts
@@ -1,3 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
 
-Route.get('/posts', 'PostsController.index')
+Route.get('/posts', async () => {
+  return Database.from('answers').select('*')
+})
